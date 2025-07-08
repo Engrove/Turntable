f4ced834db85dd6e57cff98cb50995323107750d
Engrove <jan-eric.enlund@multi.fi>
2025-07-08 17:34:28 +0300

    Update ResultsPanel.vue

--- src/components/ResultsPanel.vue
+++ src/components/ResultsPanel.vue
@@ -1,22 +1,29 @@
+<script setup>
+import { useTonearmStore } from '@/stores/tonearmStore'
+const store = useTonearmStore()
+</script>
+
 <template>
-  <div class="panel">
+  <div class="results-panel panel">
     <h2>Calculated Results</h2>
 
     <div class="result-item">
         <span class="label">Total Front Mass (m1):</span>
         <span class="value">{{ store.m1.toFixed(1) }} g</span>
     </div>
-    <div class="result-item" v-if="results">
+    <div class="result-item">
         <span class="label">Adj. CW Distance (D):</span>
-        <span class="value">{{ results.isUnbalanced ? 'Unbalanced' : `${results.L4_adj_cw.toFixed(1)} mm` }}</span>
+        <span class="value">
+            {{ store.calculatedResults.isUnbalanced ? 'Unbalanced' : `${store.calculatedResults.L4_adj_cw.toFixed(1)} mm` }}
+        </span>
     </div>
-    <div class="result-item" v-if="results && !results.isUnbalanced">
+    <div class="result-item">
         <span class="label">Effective Mass (M_eff):</span>
-        <span class="value">{{ results.M_eff.toFixed(1) }} g</span>
+        <span class="value">{{ store.calculatedResults.isUnbalanced ? '--' : store.calculatedResults.M_eff.toFixed(1) }} g</span>
     </div>
-    <div class="result-item" v-if="results && !results.isUnbalanced">
+    <div class="result-item">
         <span class="label">System Resonance Frequency:</span>
-        <span class="value" id="resonance-freq-value">{{ results.F.toFixed(1) }} Hz</span>
+        <span id="resonance-freq-value" class="value">{{ store.calculatedResults.isUnbalanced ? '--' : store.calculatedResults.F.toFixed(1) }} Hz</span>
     </div>
 
     <div class="diagnosis" :class="store.diagnosis.className">
@@ -24,11 +31,3 @@
     </div>
   </div>
 </template>
-
-<script setup>
-import { useTonearmStore } from '@/store/tonearmStore';
-import { computed } from 'vue';
-
-const store = useTonearmStore();
-const results = computed(() => store.calculatedResults);
-</script>
