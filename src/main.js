416b72c9548131fd170d56a90cacacca342ee901
Engrove <jan-eric.enlund@multi.fi>
2025-07-08 20:32:36 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
+import router from './router' // <-- Importera vår nya router
+
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(router) // <-- Tala om för appen att använda routern
+
 app.mount('#app')
