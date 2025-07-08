f9bdfbdaed12b4defb7c06c12434820c458f66da
Engrove <jan-eric.enlund@multi.fi>
2025-07-08 18:55:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path' // <-- Lägg till denna import för att hantera sökvägar
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Lägg till denna sektion för att definiera sökvägs-alias
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
