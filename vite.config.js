9bad11efc65540a94a4b4d7358cf9fe6664cfd1f
Engrove <jan-eric.enlund@multi.fi>
2025-07-08 19:24:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url' // <-- Importera moderna verktyg
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      // Använd den robusta metoden för att peka på src-mappen
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
