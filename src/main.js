779c301facd6df2eac20ff41261b62386227dd57
Engrove <jan-eric.enlund@multi.fi>
2025-07-08 19:25:11 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import './assets/main.css' // Importerar vår nya CSS-fil
+import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia()) // Aktiverar Pinia
+app.use(createPinia())
 app.mount('#app')
