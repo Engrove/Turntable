5c4cc01a7dcf6dc3d8b56fb55fbec1a127c538a1
Engrove <jan-eric.enlund@multi.fi>
2025-07-09 13:14:07 +0300

    Update TonearmCalculatorView.vue

--- src/views/TonearmCalculatorView.vue
+++ src/views/TonearmCalculatorView.vue
@@ -1,9 +1,11 @@
 <!-- src/views/TonearmCalculatorView.vue -->
 <script setup>
-import TonearmVisualization from '../components/TonearmVisualization.vue';
-import InputPanel from '../components/InputPanel.vue';
-import ResultsPanel from '../components/ResultsPanel.vue';
 import { useTonearmStore } from '@/store/tonearmStore';
+import InputPanel from '@/components/InputPanel.vue';
+import ResultsPanel from '@/components/ResultsPanel.vue';
+import CounterweightChart from '@/components/CounterweightChart.vue';
+import SensitivityCharts from '@/components/SensitivityCharts.vue';
+
 const store = useTonearmStore();
 </script>
 
@@ -11,15 +13,16 @@ const store = useTonearmStore();
   <div>
     <header class="app-header">
       <h1>Tonearm Resonance Calculator</h1>
-      <!-- Denna länk är nu en knapp som anropar en action i storen -->
-      <a href="#" @click.prevent="store.showHelpModal()">Help & Methodology</a>
+      <a @click.prevent="store.showHelpModal()">Help & Methodology</a>
     </header>
 
-    <!-- Denna sektion är nu en container för dina RIKTIGA grafer -->
+    <!-- En container för ALLA dina grafer högst upp -->
     <section class="visualization-container">
-      <TonearmVisualization />
+      <CounterweightChart />
+      <SensitivityCharts />
     </section>
 
+    <!-- En grid-container för input och resultat sida-vid-sida -->
     <div class="panels-container">
       <InputPanel class="panel" />
       <ResultsPanel class="panel" />
