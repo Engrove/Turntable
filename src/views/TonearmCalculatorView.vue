5028ca388c8063f4ee249b585ae4f785b2360419
Engrove <jan-eric.enlund@multi.fi>
2025-07-11 22:12:07 +0300

    Update TonearmCalculatorView.vue

--- src/views/TonearmCalculatorView.vue
+++ src/views/TonearmCalculatorView.vue
@@ -14,9 +14,10 @@ const showHelp = ref(false);
   <div class="tool-view">
     <div class="tool-header">
       <h1>Tonearm Resonance Calculator</h1>
-      <button @click="showHelp = true" class="help-button">
-        ? Help & Methodology
-      </button>
+      <!-- Ersätt den gamla knappen med denna -->
+<button @click="showHelp = true" class="icon-help-button" title="Help & Methodology">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
+</button>
     </div>
 
     <div class="main-content">
