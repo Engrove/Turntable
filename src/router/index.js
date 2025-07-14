149d8c5fd1f2080d711ea2ea43d9fdd6ee39033e
Engrove <jan-eric.enlund@multi.fi>
2025-07-14 10:28:24 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TonearmCalculatorView from '../views/TonearmCalculatorView.vue'
 import ComplianceEstimatorView from '../views/ComplianceEstimatorView.vue'
+import AlignmentCalculatorView from '../views/AlignmentCalculatorView.vue' // Ny import
 import DataExplorerView from '../views/DataExplorerView.vue'
-// NYTT (1g): Importera den nya rapportvyn
-import ReportView from '../views/ReportView.vue'
+import ReportView from '../views/ReportView.vue' // Befintlig report-vy
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,20 +27,36 @@ const router = createRouter({
       component: ComplianceEstimatorView,
       meta: { title: 'Compliance Estimator' }
     },
+    // Ny route för Alignment-verktyget
+    {
+      path: '/alignment-calculator',
+      name: 'alignment-calculator',
+      component: AlignmentCalculatorView,
+      meta: {
+        title: 'Alignment Calculator'
+      }
+    },
     {
       path: '/data-explorer',
       name: 'data-explorer',
       component: DataExplorerView,
       meta: { title: 'Data Explorer' }
     },
-    // NYTT (1g): Lägg till en dold route för rapporten
+    // Befintlig, dold route för rapporter
     {
       path: '/report',
       name: 'report',
       component: ReportView,
       // 'meta' utelämnas avsiktligt så att den inte visas i huvudmenyn
     },
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
