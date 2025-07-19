2de57d036f309d3eaaab9efe22f8e3abda1a1228
Jan-Eric Enlund <jan-eric.enlund@multi.fi>
2025-07-19 23:16:44 +0300

    Update AlignmentCalculatorView.vue

--- src/views/AlignmentCalculatorView.vue
+++ src/views/AlignmentCalculatorView.vue
@@ -55,7 +55,7 @@ store.initialize();
 class="icon-help-button"
 title="Help & Methodology"
 >
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 <circle cx="12" cy="12" r="10"></circle>
 <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
 <path d="M12 17h.01"></path>
@@ -81,7 +81,7 @@ title="Help & Methodology"
   <AlignmentInputPanel />
   
   <div class="results-and-visuals-column">
-    <AlignmentResultsPanel />
+    <AlignmentResultsPanel :tonearmPreset="store.currentTonearmPreset" />
 
     <TrackingErrorChart 
       v-if="!store.calculatedValues.error"
